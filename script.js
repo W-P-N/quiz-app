b1c27af2f940ae2fecadeb65d029ed8c6c88aee5
Parth <parthwani22@gmail.com>
2025-12-19 21:46:33 +0530

    Moved all start quiz functions to startQuiz block.

--- script.js
+++ script.js
@@ -7,13 +7,15 @@
         currentQuestionNumber: 0,
         correctlyAnswered: 0,
         totalMarks: 0,
-        questionWiseJustifications: []
+        questionWiseJustifications: [],
+        userSubmit: false
     };
     // Get DOM references
     const questionTimeSlider = document.getElementById('question-time');
     const sliderValue = document.getElementById('slider-value');
     const startQuizButton = document.getElementById('start-game-button');
     const errorSpanText = document.getElementById('error');
+    const submitAnswerButton = document.getElementById('submit-answer-button');
 
     // Event Listeners
     document.addEventListener('DOMContentLoaded', function() {
@@ -25,57 +27,148 @@
     });
     startQuizButton.addEventListener('click', startQuiz);
 
+    // submitAnswerButton.addEventListener('click', function() {
+    //     state.userSubmit = true;
+    // });
+
     async function startQuiz(e) {
         e.preventDefault();
         errorSpanText.innerHTML = "";
+        // DOM Element reference
+        const numQuestionsInput = document.getElementById('number-of-questions');
+        const minNumQuestions = numQuestionsInput.min;
+        const maxNumQuestions = numQuestionsInput.max;
 
-        // DOM references:
-        const numberOfQuestionsInput = document.getElementById('number-of-questions');
-
-        try {
-            // Check if the values enterd by the user are in range
-            checkStartQuizParams(numberOfQuestionsInput);
-            // Make quiz screen visible and start screen hidden
-            makeQuizVisible();
-            // Get Random Question from the json file
-            await getRandomQuestionsInArray(numberOfQuestionsInput.value, numberOfQuestionsInput.min, numberOfQuestionsInput.max);
-            // Timer display
-            startTimer();
-            // Show question
-            displayQuestion();
-            // Initialize timer
-            // Display 
-        } catch (error) {
-            console.error('Error: ', error);
-            errorSpanText.innerHTML = error;
+        // Functions
+        function validateNumberOfQuestions(numberOfQuestions, min, max) {
+            if(!numberOfQuestions || numberOfQuestions < 5 || numberOfQuestions > 100) {
+                throw new Error(`Please select minimum ${min} and maximum ${max} as number of questions.`)
+            };
+            console.log(numberOfQuestions.value, questionTimeSlider.value)  // Debug Log
             return;
         };
-    };
+        async function getRandomQuestionsInArray(numQuestionsValue, min, max) {
+            const count = parseInt(numQuestionsValue);
+            const parsedMin = parseInt(min);
+            const parsedMax = parseInt(max);
 
-    async function getRandomQuestionsInArray(numQuestionsValue, min, max) {
-        const count = parseInt(numQuestionsValue);
-        const parsedMin = parseInt(min);
-        const parsedMax = parseInt(max);
+            // Create set of unique numbers
+            const uniqueNums = new Set();
+            while(uniqueNums.size < count) {
+                const randomNum = parseInt(Math.floor(Math.random() * (parsedMax - parsedMin))) + parseInt(parsedMin);
+                uniqueNums.add(randomNum);
+            };
 
-        // Create set of unique numbers
-        const uniqueNums = new Set();
-        while(uniqueNums.size < count) {
-            const randomNum = parseInt(Math.floor(Math.random() * (parsedMax - parsedMin))) + parseInt(parsedMin);
-            uniqueNums.add(randomNum);
+            // Convert set to array
+            const uniqueNumArr = Array.from(uniqueNums);
+            // console.log(uniqueNumArr);  // Debug log
+            // Get unique data
+            const data = await fetchData();
+            // console.log(data);  // Debug log
+            for(let idx of uniqueNumArr) {
+                state.questionsArray.push(data[idx]);
+            };
+            return;
+        };
+        function makeQuizScreenVisible() {
+            const quizScreen = document.getElementById('quiz-screen');
+            const startScreen = document.getElementById('start-screen');
+
+            startScreen.classList.add('hidden');
+            quizScreen.classList.remove('hidden');
+            return;
+        };
+        function displayTimer(counter) {
+            const minutesDisplay = document.getElementById('minutes-display');
+            const secondsDisplay = document.getElementById('seconds-display');
+            
+            let seconds = counter % 60;
+            let minutes = Math.floor(counter / 60);
+            minutesDisplay.textContent = minutes;
+            secondsDisplay.textContent = seconds;
+            return;
+        };
+        function startTimer() {
+            state.counter = parseInt(questionTimeSlider.value) * 60;
+
+            state.timeInterval = setInterval(function() {
+                state.counter-=1;
+                if(state.counter <= 0) {
+                    clearInterval(state.timeInterval);
+                    onQuestionEnd();
+                };
+
+                displayTimer(state.counter);
+            }, 1000);
+        };
+        function displayQuestion() {
+            const questionsForm = document.getElementById('questions-container-form');
+
+            // Question Display
+            const questionBlock = document.getElementById('question-block');
+
+            const currentQuestion = state.questionsArray[state.currentQuestionNumber];
+            
+            questionBlock.textContent = currentQuestion.question;
+
+            // Options Display
+            Object.entries(currentQuestion.options).forEach(([key, value]) => {
+                const optionDiv = document.createElement('div');
+                optionDiv.classList.add('option-div');
+                
+                const inputElement = document.createElement('input');
+                inputElement.type = 'radio';
+                inputElement.name = 'option';
+                inputElement.id = key;
+                inputElement.value = value;
+
+                optionDiv.appendChild(inputElement);
+
+                const optionLabel = document.createElement('label');
+                optionLabel.htmlFor = key;
+                optionLabel.textContent = value;
+
+                optionDiv.appendChild(optionLabel);
+
+                questionsForm.appendChild(optionDiv);
+            });
+        };
+        function checkAnswer() {
+
         };
 
-        // Convert set to array
-        const uniqueNumArr = Array.from(uniqueNums);
-        // console.log(uniqueNumArr);  // Debug log
-        const questionsArr = [];
-        // Get unique data
-        const data = await fetchData();
-        // console.log(data);  // Debug log
-        for(let idx of uniqueNumArr) {
-            state.questionsArray.push(data[idx]);
-        };
 
-        return;
+        try {
+            // DOM references:
+            state.totalQuestions = parseInt(numQuestionsInput.value);
+            state.currentQuestionNumber = 0;
+            state.counter = 0;
+            // Check whether the number of questions are within the range
+            validateNumberOfQuestions(state.totalQuestions, minNumQuestions, maxNumQuestions);
+            // Get Questions in State array
+            await getRandomQuestionsInArray(state.totalQuestions, minNumQuestions, maxNumQuestions);
+            // Make screen quiz visible
+            makeQuizScreenVisible();
+
+            function showNextQuestion() {
+                if(state.currentQuestionNumber >= state.totalQuestions) {
+                    finishQuiz();
+                    return;
+                };
+                displayQuestion();
+                startTimer();
+            };
+
+            function finishQuiz() {
+
+            }
+
+            showNextQuestion();
+            
+        } catch (error) {
+            errorSpanText.innerHTML = error;
+            stateReset();
+        }
     };
 
     async function fetchData() {
@@ -88,80 +181,17 @@
             })
     };
 
-    function checkStartQuizParams(numberOfQuestions) {
-        if(!numberOfQuestions.value || numberOfQuestions.value < 5 || numberOfQuestions.value > 100) {
-            throw new Error(`Please select minimum ${numberOfQuestions.min} and maximum ${numberOfQuestions.max} as number of questions.`)
+    function stateReset() {
+        state = {
+            counter: 0,
+            timeInterval: null,
+            totalQuestions: 0,
+            questionsArray: [],
+            currentQuestionNumber: 0,
+            correctlyAnswered: 0,
+            totalMarks: 0,
+            questionWiseJustifications: []
         };
-        console.log(numberOfQuestions.value, questionTimeSlider.value)  // Debug Log
-        return;
-    };
-
-    function makeQuizVisible() {
-        const quizScreen = document.getElementById('quiz-screen');
-        const startScreen = document.getElementById('start-screen');
-
-        startScreen.classList.add('hidden');
-        quizScreen.classList.remove('hidden');
-        return;
-    };
-
-    function startTimer() {
-        state.counter = parseInt(questionTimeSlider.value) * 60;
-
-        state.timeInterval = setInterval(function() {
-            if(state.counter <= 0) {
-                clearInterval(state.timeInterval);
-            };
-
-            displayTimer(state.counter);
-            state.counter-=1;
-        }, 1000);
-
-    };
-
-    function displayTimer(counter) {
-        const minutesDisplay = document.getElementById('minutes-display');
-        const secondsDisplay = document.getElementById('seconds-display');
-        
-        let seconds = counter % 60;
-        let minutes = Math.floor(counter / 60);
-        minutesDisplay.textContent = minutes;
-        secondsDisplay.textContent = seconds;
-        return;
-    };
-
-    function displayQuestion() {
-        const questionContainer = document.getElementById('question-container');
-        const optionsContainerForm = document.getElementById('options-container-form');
-
-        const currentQuestion = state.questionsArray[state.currentQuestionNumber];
-        state.currentQuestionNumber+=1;
-
-        const questionElement = document.createElement('h3');
-        questionElement.textContent = currentQuestion.question;
-        questionContainer.appendChild(questionElement);
-
-        const optionContainers = [];
-        Object.entries(currentQuestion.options).forEach(([key, value]) => {
-            const optionContainer = document.createElement('input');
-            const optionLabel = document.createElement('label');
-
-            optionContainer.type = 'radio';
-            optionContainer.id = key;
-            optionContainer.value = key;
-            optionContainer.name = 'option';
-
-            optionLabel.id = key;
-            optionLabel.htmlFor = key;
-            optionLabel.textContent = value;
-
-            optionContainers.push(optionContainer);
-            optionContainers.push(optionLabel);
-        });
-
-        optionContainers.forEach(option => {
-            optionsContainerForm.appendChild(option);
-        });
     };
     
 }) ();
